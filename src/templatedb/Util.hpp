6855b98834bbea19f53e02098026599d58b1aed3
Lightbringer <lighter0610@gmail.com>
2022-05-06 22:26:21 -0400

    update compact

--- src/templatedb/Util.hpp
+++ src/templatedb/Util.hpp
@@ -5,84 +5,85 @@
 #include "Option.hpp"
 #include "SSTable.hpp"
 #include <vector>
-#include <map>
+#include <queue>
 #include <algorithm>
 #include <iterator>
 #include <iostream>
 #include <fstream>
 #include <sstream>
+typedef std::pair<std::string, int> pi;
 
 namespace Util {
+    inline int getKey(const std::string& s) {
+        std::stringstream stream(s);
+        std::string _key;
+        int key = -1;
+        stream >> key;
+        return key;
+    }
+
+    struct comp {
+        constexpr bool operator() (pi const& a, pi const& b) const noexcept {
+            int keya = getKey(a.first);
+            int keyb = getKey(b.first);
+            if (keya != keyb)
+                return keya > keyb;
+            else
+                return a.second < b.second;
+        }
+    };
+
     // Compact several unordered SSTables to ordered SSTables and delete duplicated items.
     inline std::vector<SSTable> compact(const std::vector<SSTable> &inputs, const std::string& dir, uint64_t &no) {
+        std::priority_queue<pi, std::vector<pi>, comp> heap;
         std::vector<SSTable> ret;
         int size = 0;
-        std::vector<std::ifstream> ifs;
-        std::vector<std::string> lines;
-        std::vector<int> keys;
+        int previous_min = -1;   // Check if there are two same keys from different tables.
+        std::vector<std::ifstream*> ifs;
         std::ofstream file;
         // If there's only one table, no need to compact.
         if (inputs.size() == 1)
             return inputs;
         // Create ifstream for each SSTable, save the first line and its key.
-        for (const auto& t : inputs) {
-            std::ifstream ifile(t.sstbId.name());
-            ifs.emplace_back(std::move(ifile));
+        for (int i = 0; i < inputs.size(); ++i) {
+            auto* ifile = new std::ifstream(inputs[i].sstbId.name());
+            ifs.emplace_back(ifile);
             std::string line;
-            std::getline(ifile, line);
-            std::stringstream linestream(line);
-            lines.emplace_back(line);
-            int key;
-            linestream >> key;
-            keys.emplace_back(key);
+            std::getline(**ifs.rbegin(), line);
+            // First element is key, second is the table it from.
+            heap.push(std::make_pair(line, i));
         }
         // Flag indicates if all tables are looped to the end.
-        bool flag = true;
-        while (flag) {
-            int min_idx = 0;
-            // Create a new SSTable if previous is full, or it's start the loop.
+        while (!heap.empty()) {
             if (size == 0) {
+                file.close();
                 SSTable t(SSTableId(dir, no++));
                 ret.emplace_back(t);
                 file.open(t.sstbId.name());
             }
-            // Get the smallest key index from all SSTables.
-            // From back to front since tables at the end is the newest.
-            min_idx = int(std::min_element(keys.rbegin(), keys.rend()) - keys.rend() - 1);
-            // Write the line to new file.
-            // If minimum is INT_MAX, all files are read to the end.
-            if (keys[min_idx] != INT_MAX) {
-                file << lines[min_idx];
+            pi cur = heap.top();
+            int key = getKey(cur.first);
+            if (key != previous_min) {
+                file << cur.first << std::endl;
+                if (size == 0)
+                    ret.rbegin()->min_key = key;
                 ++size;
-                // If size reach limit
                 if (size == Option::MEM_SPACE) {
                     ret.rbegin()->setSpace(size);
+                    ret.rbegin()->max_key = key;
                     size = 0;
                 }
             }
-            else {
-                ret.rbegin()->setSpace(size);
-                flag = false;
-            }
-            // Loop over the lines to drop duplicated keys and update the vectors.
-            for (int i = 0; i < keys.size(); ++i) {
-                std::string line;
-                int key;
-                if (keys[i] == keys[min_idx]) {
-                    std::getline(ifs[i], line);
-                    // If file is not end
-                    std::stringstream linestream(line);
-                    lines[i] = line;
-                    linestream >> key;
-                    // Reading the same key indicating reading the same line,
-                    // which means it's the last line of the file.
-                    if (keys[i] == key)
-                        keys[i] = INT_MAX;
-                    else
-                        keys[i] = key;
-                }
-            }
+            previous_min = key;
+            heap.pop();
+            // Read a new line to heap
+            std::string line;
+            std::getline(*(ifs[cur.second]), line);
+            if (!line.empty())
+                heap.push(std::make_pair(line, cur.second));
         }
+
+        ret.rbegin()->setSpace(size);
 
         return ret;
     }
